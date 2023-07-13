27efc2514be191c7392c7fb40110f98444e97c08
mbeka02 <antonymbeka@gmail.com>
2023-07-14 00:34:55 +0300

    solution added

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+/*Given two crystal balls that will break if dropped from
+ a high enough distance, determine the exact spot in which it will
+  break in the most optimized way*/
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+    //jump should be sqrt of arr length , ensure it's a whole number
+    const jump_amount = Math.floor(Math.sqrt(breaks.length));
+    let i = jump_amount;
+    //check for breaks
+    for (; i < breaks.length; i += jump_amount) {
+        if (breaks[i] === true) {
+            break;
+        }
+    }
+    //go to last point before the ball breaks
+    i -= jump_amount;
+    //find exact index of the ball breaking
+    for (let j = 0; j <= jump_amount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
