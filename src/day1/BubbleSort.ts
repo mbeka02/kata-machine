ba5008f7543573a43ce247723cc4d5e85dcd7362
mbeka02 <antonymbeka@gmail.com>
2023-07-14 21:27:09 +0300

    implemented bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let tmp;
+    const n = arr.length;
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n - 1 - i; ++j) {
+            //check if next num is smaller and swap if so
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
